e4889aef3adaf4d363913d8746e28a2022f6e1ba
msherchan <67372791+msherchan@users.noreply.github.com>
2020-08-11 22:12:56 -0600

    Update DBUtil.java

--- src/main/java/com/vastika/BankingApp/util/DBUtil.java
+++ src/main/java/com/vastika/BankingApp/util/DBUtil.java
@@ -9,8 +9,8 @@ public class DBUtil {
 	
 	public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/BankingApp?serverTimezone=UTC";
-	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root1234";
+	public static final String USER_NAME = "";
+	public static final String PASSWORD = "";
 	
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
